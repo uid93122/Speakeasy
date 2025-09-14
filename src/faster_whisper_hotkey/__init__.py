885f5cb132009bb16384e6480a4efe6c87dc7f87
blakkd <blakkd@protonmail.com>
2025-09-14 02:57:23 +0200

    v0.4.0

--- src/faster_whisper_hotkey/__init__.py
+++ src/faster_whisper_hotkey/__init__.py
@@ -4,4 +4,4 @@ faster-whisper-hotkey: Push-to-talk transcription.
 Hold the hotkey, Speak, Release ==> And baamm in the currently focused text field!
 """
 
-__version__ = "0.3.8"
+__version__ = "0.4.0"
